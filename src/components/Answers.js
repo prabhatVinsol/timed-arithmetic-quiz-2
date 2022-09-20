90383893713c8733197b95f4629c16446b02b1dc
Prabhat Ranjan <prabhat@vinsol.com>
2022-09-20 20:07:54 +0530

    Simplify the logic of filter answers

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -4,7 +4,7 @@ function Answers(props) {
   const { answers, shouldShowCorrectAnswers } = props;
 
   const getCorrectOrWrongAnswerList = answers.filter((answer) => (
-    shouldShowCorrectAnswers ? answer.correct : !answer.correct));
+    shouldShowCorrectAnswers === answer.correct));
   return (
     <div>
       <h2>
